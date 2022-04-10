6dc843d289c10fc01638811dd64a524fb8c75be5
Dariel De Jesus <darieldejesus@gmail.com>
2022-04-11 01:27:54 +0200

    Agregar funcion para cargar las detenciones (#3)

---
+++ src/resolvers/queries/detection.ts
@@ -0,0 +1,31 @@
+import DB from "../../config/database";
+import File from "../../models/file";
+import Detection from "../../models/detection";
+
+interface Result {
+  fileName: string;
+  path: string;
+  lines: string[];
+}
+
+interface Params {
+  fileName: string;
+}
+
+const PresignedUrl = async (_: unknown, params: Params): Promise<Result> => {
+  const { fileName } = params;
+  const knex = await DB.getInstance().getKnex();
+  const fileEntry = await File.query(knex).findOne({ name: fileName });
+  if (!fileEntry) {
+    throw new Error("File not found");
+  }
+  const detections = await Detection.query(knex).where("file_id", fileEntry.id);
+  const lines = detections.map((entry) => entry.status);
+  return {
+    fileName: fileEntry.name,
+    path: fileEntry.path,
+    lines,
+  };
+};
+
+export default PresignedUrl;
